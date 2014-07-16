af211847c6d1baf590fbab641a887fb1051cfb25
ken restivo <ken@restivo.org>
2014-07-16 00:33:00 -0700

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to podload
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/podload/core_test.clj
@@ -0,0 +1,7 @@
+(ns podload.core-test
+  (:require [clojure.test :refer :all]
+            [podload.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
